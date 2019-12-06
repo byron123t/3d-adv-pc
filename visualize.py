3f46779d0756080b0b08679061d11820950a0b65
Brian <bjtang2@plato.engr.wisc.edu>
2019-12-05 19:05:07 -0600

    Add requirements.txt and update visualize.py

--- visualize.py
+++ visualize.py
@@ -6,16 +6,16 @@ import os
 
 
 def load(num_points):
-	y = pd.read_csv('data/labels/labels{}.csv'.format(num_points), header=None).values
-	z = pd.read_csv('data/metadata/metadata{}.csv'.format(num_points)).values
+	y = pd.read_csv('data/points/labels{}.csv'.format(num_points), header=None).values
+	z = pd.read_csv('data/points/metadata{}.csv'.format(num_points)).values
 	x = np.load('data/points/points{}.npy'.format(num_points))
 	return x, y, z
 
 
-def load_augment(num_points):
-	y = pd.read_csv('data/labels/augment{}.csv'.format(num_points), header=None).values
+def load_augment():
+	y = pd.read_csv('data/points/augment.csv', header=None).values
 	z = []
-	x = np.load('data/points/augment{}.npy'.format(num_points))
+	x = np.load('data/points/augment.npy')
 	return x, y, z
 
 
@@ -42,39 +42,33 @@ def vis(x, y, z):
 	open3d.visualization.draw_geometries([cloud])
 
 
-def main():
+def visualize_adversarial():
 	points = ['0_1_0_adv.npy', '0_1_0_orig.npy', '0_2_0_adv.npy', '0_2_0_orig.npy', '0_3_0_adv.npy', '0_3_0_orig.npy',
-		'1_0_0_adv.npy', '1_0_0_orig.npy', '1_2_0_adv.npy', '1_2_0_orig.npy', '1_3_0_adv.npy', '1_3_0_orig.npy',
-		'2_0_0_adv.npy', '2_0_0_orig.npy', '2_1_0_adv.npy', '2_1_0_orig.npy', '2_3_0_adv.npy', '2_3_0_orig.npy',
-		'3_0_0_adv.npy', '3_0_0_orig.npy', '3_1_0_adv.npy', '3_1_0_orig.npy', '3_2_0_adv.npy', '3_2_0_orig.npy']
-
-#	points = ['0_1_0_orig.npy', '0_2_0_orig.npy', '0_3_0_orig.npy',
-#                '1_0_0_orig.npy', '1_2_0_orig.npy', '1_3_0_orig.npy',
-#                '2_0_0_orig.npy', '2_1_0_orig.npy', '2_3_0_orig.npy',
-#                '3_0_0_orig.npy', '3_1_0_orig.npy', '3_2_0_orig.npy']
-
-	# x, y, z = load('_crop')
-	# x_f, y_f, z_f = load('_full')
-	# x, y, z = load('32')
-#	for i in os.listdir('perturbation/'):
+	'1_0_0_adv.npy', '1_0_0_orig.npy', '1_2_0_adv.npy', '1_2_0_orig.npy', '1_3_0_adv.npy', '1_3_0_orig.npy',
+	'2_0_0_adv.npy', '2_0_0_orig.npy', '2_1_0_adv.npy', '2_1_0_orig.npy', '2_3_0_adv.npy', '2_3_0_orig.npy',
+	'3_0_0_adv.npy', '3_0_0_orig.npy', '3_1_0_adv.npy', '3_1_0_orig.npy', '3_2_0_adv.npy', '3_2_0_orig.npy']
 	for i in points:
 		x, y, z = load_adversarial(i)
 		print(i)
 		vis(x, y, z)
-		#LENGTH = len(x)
-		#rand = []
-		#for i in range(LENGTH):
-		#	rand.append(i)
-		#random.shuffle(rand)
-		#for i in rand:
-			#visualize(i, x_f, y_f, z_f)
-			#print(x[i].shape)
-			#z_axis = []
-			#for j in x[i]:
-			#	z_axis.append(j[0])
-			#z_axis.sort()
-			#print(z_axis)
-		#	visualize(i, x, y, z)
+
+
+def visualize_normal():
+	x, y, z = load('_crop')
+	x_f, y_f, z_f = load('_full')
+	length = len(x)
+	rand = []
+	for i in range(length):
+		rand.append(i)
+	random.shuffle(rand)
+	for i in rand:
+		visualize(i, x, y, z)
+		print(x[i].shape)
+		visualize(i, x_f, y_f, z_f)
+
+
+def main():
+	visualize_normal()
 
 
 if __name__ == '__main__':
